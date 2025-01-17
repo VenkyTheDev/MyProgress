f2ce31ebe4c85ee6792c7cc9932fc2cd9a4c1e96
User OnGrid <userongrid@Admins-MacBook-Pro-6.local>
2025-01-17 15:22:45 +0530

    Finished all the challenges of Section 5

--- src/main/java/Day2/RemainingChallenges.java
+++ src/main/java/Day2/RemainingChallenges.java
@@ -11,7 +11,25 @@ public class RemainingChallenges {
     public static boolean isTeen(int a){
         return ((a >= 13) && (a <= 19));
     }
-
+    public static void printYearsAndDays(long minutes){
+        if(minutes < 0) System.out.print("Invalid Value");
+        else{
+            long year = (((minutes / 60L) / 24L) / 365L);
+            long days = (minutes / 60L / 24L) - (year * 365L);
+            System.out.print(minutes + " min = " + year + " y " + "and " + days + " d");
+        }
+    }
+    public static void printEqual(int a , int b , int c){
+        if(a < 0 || b < 0 || c < 0) System.out.print("Invalid Value");
+        else if((a == b) && (a == c)) System.out.print("All numbers are equal");
+        else if(((a != b) && (a != c)) && (b != c)) System.out.print("All numbers are different");
+        else System.out.print("Neither all are equal or different");
+    }
+    public static boolean isCatPlaying(boolean summer , int temperature){
+        if(summer && (temperature >= 25 && temperature <= 45)) return true;
+        else if(!summer && (temperature >= 25 && temperature <= 35)) return true;
+        return false;
+    }
     public static void main(String[] args) {
         System.out.println();
     }
