23f9b6452e3ef5eee793a846b6b89f16d8bfaaed
Venkata Krishna Polisetty <venkata.krishna@ongrid.in>
2025-01-21 10:18:59 +0530

    Finished all the challenges till Section 11

--- src/main/java/Day4/Section11.java
+++ src/main/java/Day4/Section11.java
@@ -1,5 +1,5 @@
 package Day4;
-
+import java.util.*;
 public class Section11 {
     //Abstract Class
     public abstract class ListItem {
@@ -250,5 +250,136 @@ public class Section11 {
             }
         }
     }
+    public class Player implements ISaveable {
+        // write code here
+        private String name;
+        private String weapon;
+        private int hitPoints;
+        private int strength;
 
+        public Player(String name, int hitPoints, int strength) {
+            this.name = name;
+            this.hitPoints = hitPoints;
+            this.strength = strength;
+            this.weapon = "Sword";
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getWeapon() {
+            return weapon;
+        }
+
+        public void setWeapon(String weapon) {
+            this.weapon = weapon;
+        }
+
+        public int getHitPoints() {
+            return hitPoints;
+        }
+
+        public void setHitPoints(int hitPoints) {
+            this.hitPoints = hitPoints;
+        }
+
+        public int getStrength() {
+            return strength;
+        }
+
+        public void setStrength(int strength) {
+            this.strength = strength;
+        }
+
+        @Override
+        public List<String> write() {
+            List<String> values = new ArrayList<>();
+            values.add(this.name);
+            values.add(String.valueOf(this.hitPoints));
+            values.add(String.valueOf(this.strength));
+            values.add(this.weapon);
+            return values;
+        }
+
+        @Override
+        public void read(List<String> savedValues) {
+            if (savedValues != null && savedValues.size() > 0) {
+                this.name = savedValues.get(0);
+                this.hitPoints = Integer.parseInt(savedValues.get(1));
+                this.strength = Integer.parseInt(savedValues.get(2));
+                this.weapon = savedValues.get(3);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Player{name='" + name + '\'' +
+                    ", hitPoints=" + hitPoints +
+                    ", strength=" + strength +
+                    ", weapon='" + weapon + '\'' +
+                    '}';
+        }
+    }
+    public class Monster implements ISaveable {
+        private String name;
+        private int hitPoints;
+        private int strength;
+
+        public Monster(String name, int hitPoints, int strength) {
+            this.name = name;
+            this.hitPoints = hitPoints;
+            this.strength = strength;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getHitPoints() {
+            return hitPoints;
+        }
+
+        public int getStrength() {
+            return strength;
+        }
+
+        @Override
+        public List<String> write() {
+            List<String> values = new ArrayList<>();
+            values.add(this.name);
+            values.add(String.valueOf(this.hitPoints));
+            values.add(String.valueOf(this.strength));
+            return values;
+        }
+
+        @Override
+        public void read(List<String> savedValues) {
+            if (savedValues != null && savedValues.size() > 0) {
+                this.name = savedValues.get(0);
+                this.hitPoints = Integer.parseInt(savedValues.get(1));
+                this.strength = Integer.parseInt(savedValues.get(2));
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Monster{name='" + name + '\'' +
+                    ", hitPoints=" + hitPoints +
+                    ", strength=" + strength +
+                    '}';
+        }
+    }
+    public interface ISaveable {
+        List<String> write();
+        void read(List<String> savedValues);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
 }
